c9c16741f7de327f0cb65b32c93aa738bf87847b
SolvedRex <jeep.escobedo@gmail.com>
2018-09-06 16:50:31 -0500

    Add files via upload

--- Linked-List/src/src/MyLinkedList.java
+++ Linked-List/src/src/MyLinkedList.java
@@ -1,14 +1,12 @@
-//A01350928
-//Javier Emiliano Escobedo Padilla
-//05/09/2018
-//Estructura de Datos Grupo 1
 package src;
 
-public class MyLinkedList<E> {
+import java.util.NoSuchElementException;
+
+public class MyLinkedList <E>{
 	private Node<E> head,
-				 tail;
+	tail;
 	private int size;
-	
+
 	public MyLinkedList () {
 		this.head = this.tail = null;
 		this.size = 0;
@@ -16,7 +14,8 @@ public class MyLinkedList<E> {
 	//Tarea: convertir arreglo a linkedlist
 	public MyLinkedList (E[]elements) {
 		Node <E> next = new Node <> (null);
-		for (int i = 0; i < elements.length;i++) {
+		size = elements.length;
+		for (int i = 0; i < size;i++) {
 			if (i == 0) {
 				Node <E> nvo = new Node<> (elements [i]);
 				head = nvo;
@@ -33,20 +32,20 @@ public class MyLinkedList<E> {
 			}
 		}
 	}
-	
+
 	public E first () {
-		return (E) this.head.getData();
+		return this.head.getData();
 	}
-	
+
 	public E last ()
 	{
 		return this.tail.getData();
 	}
-	
+
 	public boolean isEmpty() {
 		return this.size == 0;
 	}
-	
+
 	public void insertAtFirst (E data) {
 		Node<E> nvo = new Node<> (data,head);
 		this.head = nvo;
@@ -54,21 +53,21 @@ public class MyLinkedList<E> {
 			this.tail = nvo;
 		}
 		this.size++;
-		
+
 	}
-	
+
 	public void addLast (E data) {
 		if (this.size != 0) {
-		Node<E> nvo = new Node<> (data,null);
-		this.tail.setNext(nvo);
-		this.tail = nvo;
-		this.size++;
+			Node<E> nvo = new Node<> (data,null);
+			this.tail.setNext(nvo);
+			this.tail = nvo;
+			this.size++;
 		}
 		else {
 			this.insertAtFirst(data);
 		}
 	}
-	
+
 	//Tarea
 	public void insertAt (E data, int pos) throws IndexOutOfBoundsException { //Si pos < 0 o mayor a size
 		if (pos < 0 || pos > size) {
@@ -88,13 +87,77 @@ public class MyLinkedList<E> {
 		}
 	}
 	
-	public static void main(String[] args) {
+	public E removeFirst () throws NoSuchElementException {
+		try {
+		E tmp = this.head.getData();
+		this.head = this.head.next();
+		if (this.size == 1) {
+			this.tail= null;
+		}
+		this.size--;
+		System.out.println(tmp);
+		return tmp;
+		}
+		catch (NullPointerException ex) {
+			throw new NoSuchElementException ("You can´t remove the first element of an empty list");
+		}
+	}
+	
+	public E removeLast ()  throws NoSuchElementException{
+		try{
+			E tmp = this.tail.getData();
+			Node <E> next = head;
+			while (next.next() != this.tail) {
+				next = next.next();
+			}
+			this.tail = next;
+			this.tail.setNext(null);
+			if (this.size == 1) {
+				this.head= null;
+			}
+			this.size--;
+			System.out.println(tmp.toString());
+			return tmp;
+		}
+		catch (NullPointerException ex) {
+			throw new NoSuchElementException("You can´t remove the last element of an empty list");
+		}
+	}
+	
+	public String toString() {
+		String tmp = "";
+		Node <E> current = this.head;
+		for (int i = 0; i < size; i++) {
+			if (i == 0) {
+				tmp = tmp + current.getData();
+			}
+			tmp = tmp + "," + current.getData();
+			current = current.next();
+		}
+		return tmp;
+	}
+	
+	public E removeAt(int x) {
 		
+	}
+	public E getAt (int x) {
+		
+	}
+
+	public static void main(String[] args) {
+		MyLinkedList <Integer> lista = new MyLinkedList<>();
+		for (int i = 0;i < 10; i++) {
+			lista.insertAtFirst(i*5);
+		}
+		System.out.println(lista.toString());
+		lista.removeFirst();
+		lista.removeLast();
+		System.out.println(lista.toString());
 	}
 }
 
 class Node <E>{
-	
+
 	private E data;
 	private Node<E> ref;
 
@@ -128,6 +191,7 @@ class Node <E>{
 	public Node(E data) {
 		this(data,null);
 	}
-	
-	
+	public String toString () {
+		return this.getData().toString();
+	}
 }
